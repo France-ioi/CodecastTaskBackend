a90a3ac49236c4b525f20b007da352cf0fc4d98a
Sébastien Tainon <arkandias1@hotmail.fr>
2022-09-26 18:10:12 +0200

    Add linter

---
+++ .eslintrc.js
@@ -0,0 +1,69 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 2017,
+    sourceType: "module",
+  },
+  rules: {
+    indent: ['error', 2, { "SwitchCase": 1 }],
+    "linebreak-style": ["error", "unix"],
+    "no-console": "warn",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { vars: "all", args: "after-used", ignoreRestSiblings: false },
+    ],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/explicit-function-return-type': ['error'],
+    "no-empty": "warn",
+
+    'no-unused-vars': 'off',
+    '@typescript-eslint/class-literal-property-style': [
+      'error',
+      'fields',
+    ],
+    '@typescript-eslint/prefer-for-of': ['error'],
+    '@typescript-eslint/semi': ['error'],
+    '@typescript-eslint/member-delimiter-style': ['error', {
+      "multiline": { "delimiter": "comma", "requireLast": true },
+      "singleline": { "delimiter": "comma", "requireLast": false },
+      "multilineDetection": "last-member"
+    }],
+    'arrow-parens': ['error', 'as-needed'],
+    'arrow-body-style': ['error', 'as-needed'],
+    'no-confusing-arrow': ['error'],
+    'arrow-spacing': ['error', { "before": true, "after": true }],
+    'brace-style': ['error', '1tbs'],
+    'object-curly-spacing': ['error', 'never'],
+    'array-bracket-spacing': ['error', 'never', { 'objectsInArrays': false }],
+    'computed-property-spacing': ['error'],
+    'space-in-parens': ['error'],
+    'func-call-spacing': ['error'],
+    'no-trailing-spaces': ['error'],
+    'no-multi-spaces': ['error'],
+    'block-spacing': ['error'],
+    'key-spacing': ['error'],
+    'keyword-spacing': ['error'],
+    'no-eq-null': ['error'],
+    '@typescript-eslint/strict-boolean-expressions': ['error', {
+      allowNullableString: true,
+      allowNullableNumber: true,
+      allowNullableBoolean: true
+    }],
+    'quotes': ['error', 'single', {
+      avoidEscape: true,
+      allowTemplateLiterals: false
+    }],
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-import { init, start } from "./server";
-import path from "path";
-import * as dotenv from "dotenv";
-import * as Db from "./db";
+import {init, start} from './server';
+import path from 'path';
+import * as dotenv from 'dotenv';
+import * as Db from './db';
 
 dotenv.config({path: path.resolve(__dirname, '../.env.local')});
 dotenv.config({path: path.resolve(__dirname, '../.env')});
 
 Db.init();
 
-init().then(() => start());
+init();
+start();
